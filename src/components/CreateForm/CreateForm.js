a0e6f9ee2ce91b987949e239c4c34c0f9e6fcec7
Ed Ignot <edignot@gmail.com>
2020-09-04 12:27:33 -0600

    Geolocation coordinates added

--- src/components/CreateForm/CreateForm.js
+++ src/components/CreateForm/CreateForm.js
@@ -3,6 +3,23 @@ import Input from '../../UIComponents/Input/Input'
 import Button from '../../UIComponents/Button/Button'
 
 const CreateForm = () => {
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      return navigator.geolocation.getCurrentPosition(showPosition)
+    } else {
+      alert('Geo Location is not supported by your device')
+    }
+  }
+
+  const showPosition = (position) => {
+    const location = {
+      longitude: position.coords.longitude,
+      latitude: position.coords.latitude,
+    }
+    console.log(location)
+    return location
+  }
+
   return (
     <form>
       <Input type='text' placeholder='art title' />
@@ -15,7 +32,7 @@ const CreateForm = () => {
         rows='5'
       />
       <Input type='text' placeholder='address' />
-      <Button to='/explore' onClick={() => alert('created')}>
+      <Button to='/explore' onClick={getLocation}>
         CREATE
       </Button>
     </form>
