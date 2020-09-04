8705bea6390a4d2b08ef6d242704a645a7679f64
Ed Ignot <edignot@gmail.com>
2020-09-04 14:50:16 -0600

    Create form update

--- src/components/CreateForm/CreateForm.js
+++ src/components/CreateForm/CreateForm.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Input from '../../UIComponents/Input/Input'
 import Button from '../../UIComponents/Button/Button'
 
 const CreateForm = () => {
-  const getLocation = () => {
+  const [currentLocation, setCurrentLocation] = useState(null)
+
+  const getLocation = async () => {
     if (navigator.geolocation) {
       return navigator.geolocation.getCurrentPosition(showPosition)
     } else {
@@ -16,25 +18,37 @@ const CreateForm = () => {
       longitude: position.coords.longitude,
       latitude: position.coords.latitude,
     }
-    console.log(location)
+    setCurrentLocation(location)
     return location
   }
 
+  const artCreateHandler = async (e) => {
+    e.preventDefault()
+    await getLocation()
+  }
+
   return (
-    <form>
-      <Input type='text' placeholder='art title' />
-      <Input type='text' placeholder='artist name' />
-      <Input type='text' placeholder='artist instagram' />
+    <form onSubmit={artCreateHandler}>
+      <Input type='text' placeholder='art title' value='' />
+      <Input type='text' placeholder='artist name' value='' />
+      <Input type='text' placeholder='artist instagram' value='' />
       <Input
         type='text'
         element='textarea'
         placeholder='description'
         rows='5'
+        value=''
       />
-      <Input type='text' placeholder='address' />
-      <Button to='/explore' onClick={getLocation}>
-        CREATE
-      </Button>
+      <Input
+        type='text'
+        placeholder='address'
+        value={
+          currentLocation
+            ? (currentLocation.latitude, currentLocation.longitude)
+            : 'no location'
+        }
+      />
+      <Button type='submit'>CREATE</Button>
     </form>
   )
 }
