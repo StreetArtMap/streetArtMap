db83d1011d0b9d291c41c6361d7bbf0841addb00
Ed Ignot <edignot@gmail.com>
2020-09-04 11:09:19 -0600

    Buttons merged

--- src/UIComponents/Button/Button.js
+++ src/UIComponents/Button/Button.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 import './Button.css'
 
 const Button = ({
@@ -42,52 +43,10 @@ const Button = ({
   )
 }
 
-import PropTypes from 'prop-types'
+Button.propTypes = {
+  type: PropTypes.string,
+  disabled: PropTypes.bool
+}
 
-// const Button = (props) => {
-//   const { children, type, disabled } = props
-//   // this allows us to pass any text as child
-//   // create different types of buttons
-//   // also we can pass in a boolean prop into disabled so if it is loading, it is disabled
-//   return (
-//     <button type={type} disabled={disabled}>
-//       {children}
+export default Button
 
-//       <style jsx>{`
-//         button {
-//           padding: 0.7em 1.7em;
-//           border: none;
-//           border-radius: 1.4em;
-//           text-transform: uppercase;
-//           font-family: 'Roboto', sans-serif;
-//           cursor: pointer;
-//           outline: none;
-//           font-size: 1em;
-//           color: pink;
-//           box-shadow: 1px 1px grey;
-//           background-color: green;
-//         }
-//         button:hover {
-//           box-shadow: 5px 4px grey;
-//         }
-//         button:active {
-//           box-shadow: 10px 10px grey;
-//         }
-//         button:disabled {
-//           color: white;
-//           box-shadow: 1px 2px brown;
-//           background-color: grey;
-//         }
-//       `}
-//       </style>  
-//     </button>
-
-//   )
-// }
-
-// Button.propTypes = {
-//   type: PropTypes.string,
-//   disabled: PropTypes.bool
-// }
-
-// export default Button
