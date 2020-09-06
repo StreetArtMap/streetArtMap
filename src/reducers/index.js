7dbda3cb3dac470cbfa61c2b90389c469010a823
Ed Ignot <edignot@gmail.com>
2020-09-05 19:29:27 -0600

    Redux store mock data setup

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { user } from './userReducer'
+import { art } from './artReducer'
 
 const rootReducer = combineReducers({
   user,
+  art,
 })
 
 export default rootReducer
