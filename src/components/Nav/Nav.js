5bfb23e28720edf2829fd0c5866f472a09b87ab7
Ed Ignot <edignot@gmail.com>
2020-09-05 21:09:27 -0600

    Nav icons added

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import './Nav.css'
 import { NavLink } from 'react-router-dom'
+import { BsFillPersonFill } from 'react-icons/bs'
+import { FaMapMarked, FaSearch } from 'react-icons/fa'
+import { ImCamera } from 'react-icons/im'
 
 const Nav = () => {
   return (
     <footer>
       <section className='nav-box'>
-        <NavLink to='/explore'>explore</NavLink>
+        <NavLink to='/explore' className='nav-link'>
+          <FaSearch className='nav-icon' />
+          <p className='nav-text'>explore</p>
+        </NavLink>
       </section>
       <section className='nav-box'>
-        <NavLink to='/map'>map</NavLink>
+        <NavLink to='/map' className='nav-link'>
+          <FaMapMarked className='nav-icon' />
+          <p className='nav-text'>map</p>
+        </NavLink>
       </section>
       <section className='nav-box'>
-        <NavLink to='/create'>create</NavLink>
+        <NavLink to='/create' className='nav-link'>
+          <ImCamera className='nav-icon' />
+          <p className='nav-text'>create</p>
+        </NavLink>
       </section>
       <section className='nav-box'>
-        <NavLink to='/profile'>profile</NavLink>
+        <NavLink to='/profile' className='nav-link'>
+          <BsFillPersonFill className='nav-icon' />
+          <p className='nav-text'>profile</p>
+        </NavLink>
       </section>
     </footer>
   )
