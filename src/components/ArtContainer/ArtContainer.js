5292c11bf30469a56cf787be1fcac63253b49047
Ed Ignot <edignot@gmail.com>
2020-09-12 17:59:17 -0600

    Explore page toggle favorite and visited error handling

--- src/components/ArtContainer/ArtContainer.js
+++ src/components/ArtContainer/ArtContainer.js
@@ -3,15 +3,11 @@ import ImageCarousel from '../ImageCarousel/ImageCarousel'
 import ArtDetails from '../ArtDetails/ArtDetails'
 import './ArtContainer.css'
 
-const ArtContainer = ({ art, imageLink, setLoading }) => {
+const ArtContainer = ({ art, imageLink, setLoading, setError }) => {
   return (
     <section className='art-container'>
-      <ImageCarousel
-        images={art.images}
-        id={art.id}
-        imageLink={imageLink}
-      />
-      <ArtDetails art={art} setLoading={setLoading} />
+      <ImageCarousel images={art.images} id={art.id} imageLink={imageLink} />
+      <ArtDetails art={art} setLoading={setLoading} setError={setError} />
     </section>
   )
 }
